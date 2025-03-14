c713371e4efd08b941df8c624e759facd354e6ba
Jacob Julius <jeccopatrick@outlook.com>
2025-03-14 13:34:20 +0300

    Fetch blog data from API and display title and body in the blog component

--- app/routes/blogs.$blogId.tsx
+++ app/routes/blogs.$blogId.tsx
@@ -1,17 +1,21 @@
-import type { LoaderFunctionArgs } from "@remix-run/node";
+import { json, type LoaderFunctionArgs } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
 
 
 
 export const loader = async ({params}: LoaderFunctionArgs) => {
-    console.log(params)
-    return null;
+  const response = await fetch("https://jsonplaceholder.typicode.com/${params.blogId}");
+  const data: {title: string, body: string, id: string, userId: string} = await response.json();
+
+    return json({blog: data});
 }
 
 const Blog = () => {
-
+     const {blog} = useLoaderData<typeof loader>();
   return (
     <div>
-        Blog
+        <p>{blog.title} </p>
+        <h1> {blog.body} </h1>
     </div>
   )
 }
