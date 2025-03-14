f15830f35b284ef2399eef5d8a97761f6db3f773
Jacob Julius <jeccopatrick@outlook.com>
2025-03-14 14:02:25 +0300

    ""

--- app/routes/blogs.$blogId.tsx
+++ app/routes/blogs.$blogId.tsx
@@ -3,18 +3,14 @@ import { useLoaderData } from "@remix-run/react";
 
 
 
-export const loader = async ({params}: LoaderFunctionArgs) => {
-  const response = await fetch("https://jsonplaceholder.typicode.com/${params.blogId}");
-  const data: {title: string, body: string, id: string, userId: string} = await response.json();
 
-    return json({blog: data});
 }
 
 const Blog = () => {
      const {blog} = useLoaderData<typeof loader>();
   return (
     <div>
-      <h1>{blog.title}</h1>
+     
   
       <h2>
         hhhhhh
